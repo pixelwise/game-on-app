dc3e6f3ee4c0d5039676a758c036485bb7b2d8c4
Cyril Rutten <crutten@gmail.com>
2016-10-27 14:30:15 +0200

    wip

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         //app.receivedEvent('deviceready');
-		window.location.href = 'http://www.createk.nl/phonegap/www/index.html';
+		navigator.app.loadUrl(url, { openExternal:false }); 
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
